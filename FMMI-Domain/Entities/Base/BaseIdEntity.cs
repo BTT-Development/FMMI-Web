2a571370505afdb0863c858b20fc54296092a1ed
Cabuxito <briandigangi@hotmail.com>
2025-10-08 13:32:46 +0200

    +Entities and Base service

---
+++ FMMI-Domain/Entities/Base/BaseIdEntity.cs
@@ -0,0 +1,20 @@
+﻿using FMMI_Domain.Entities.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMMI_Domain.Entities.Base
+{
+    public class BaseIdEntity : IBaseIdEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ConcurrencyCheck]
+        public virtual string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
+
+    }
+}
