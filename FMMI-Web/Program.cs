1fa601c173d39f64018d58f8a8754bfb737a9f2b
Cabuxito <briandigangi@hotmail.com>
2025-10-08 12:27:34 +0200

    +UI ( half )

--- FMMI-Web/Program.cs
+++ FMMI-Web/Program.cs
@@ -45,11 +45,13 @@ builder.Services.AddSession(options =>
 
 var app = builder.Build();
 
+#region Create database schema if not exists
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<FMMIContext>();
-    db.Database.Migrate(); // This creates the DB and applies migrations
+    db.Database.Migrate(); // 
 }
+#endregion
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
