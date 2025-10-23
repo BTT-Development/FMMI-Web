364c8eb9591cda245b46c0b49d2eed982a856033
ProKrillz <thomasdamkjaer@gmail.com>
2025-10-23 15:39:00 +0200

    +mqtt

---
+++ FMMI-Service/DTO/Device/DeviceSettingsDTO.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace FMMI_Service.DTO.Device;
+
+public class DeviceSettingsDTO
+{
+    public int Id { get; set; }
+    public int RealtimeInterval { get; set; }
+    public int DataInterval { get; set; }
+
+    public int DeviceId { get; set; }
+
+    public string Topic { get; set; }
+}
---
+++ FMMI-Service/Services/DeviceSettingsServices/DeviceSettingsService.cs
@@ -0,0 +1,47 @@
+﻿using FMMI_Domain;
+using FMMI_Domain.Entities;
+using FMMI_Service.DTO.Device;
+using FMMI_Service.Result;
+using FMMI_Service.Services.Base;
+using Microsoft.EntityFrameworkCore;
+using MQTTnet;
+using MQTTnet.Client;
+
+namespace FMMI_Service.Services.DeviceSettingsServices;
+
+internal class DeviceSettingsService : BaseService<DeviceSettings>
+{
+    private FMMIContext _context;
+
+    private IMqttClient _mqttClient;
+
+    private readonly MqttClientOptionsBuilder _mqttClientOptionsBuilder;
+    public DeviceSettingsService(FMMIContext context, IMqttClient mqttClient, MqttClientOptionsBuilder mqttClientOption) : base(context)
+    {
+        _context = context;
+        _mqttClient = mqttClient;
+        _mqttClientOptionsBuilder = mqttClientOption;
+    }
+    public async Task<Result.Result> UpdateDeviceSettings(DeviceSettingsDTO settings)
+    {
+        DeviceSettings foundSettings = await _context.DeviceSettings.FindAsync(settings.Id);
+        if (foundSettings.RealtimeInterval != settings.RealtimeInterval)
+        {
+            await _context.DeviceSettings.Where(x => x.Id == settings.Id).ExecuteUpdateAsync(x => x.SetProperty(p => p.RealtimeInterval, settings.RealtimeInterval));
+        }
+        if (foundSettings.DataInterval != settings.DataInterval)
+        {
+            await _context.DeviceSettings.Where(x => x.Id == settings.Id).ExecuteUpdateAsync(x => x.SetProperty(p => p.DataInterval, settings.DataInterval));
+
+        }
+        MqttFactory mqttFactory = new MqttFactory();
+        using (_mqttClient = mqttFactory.CreateMqttClient())
+        {
+            await _mqttClient.ConnectAsync(_mqttClientOptionsBuilder.WithCredentials("", "").Build(), CancellationToken.None);
+            
+        
+        
+        }
+        return Result.Result.Succes("opdateret");
+    }
+}
